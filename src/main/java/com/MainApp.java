c031578e0a86572da92b818e348d71711d17b30b
Arul Anbarasan-A <anbu91943@gmail.com>
2025-03-15 11:34:58 +0530

    second commit

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -9,6 +9,5 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(MainApp.class, args);
-		System.out.println("Hello,World!");
 	}
 }
